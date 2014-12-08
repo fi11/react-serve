d5ca90eac0c77bc9fc45653784f211093810910f
fi11 <pavelsilinw@gmail.com>
2014-12-08 16:20:14 +0300

    add page mixin and move mixins to own dir

---
+++ mixins/page.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var dom = React.DOM;
+
+module.exports = {
+    generateScriptsComponent: function(scripts) {
+        return [].concat(scripts || []).map(function(item, idx) {
+            return dom.script({
+                id: item.id ? item.id : undefined,
+                dangerouslySetInnerHTML: { __html: item.src, key: idx }
+            });
+        });
+    },
+
+    generateBodyComponent: function(scripts) {
+        var body = React.renderComponentToString(this.props.appComponent()) +
+            [].concat(scripts || []).map(function(item, idx) {
+                return React.renderComponentToStaticMarkup(
+                    dom.script({
+                        id: item.id ? item.id : undefined,
+                        dangerouslySetInnerHTML: { __html: item.src, key: idx }
+                    }));
+            }).join('\n');
+
+        return dom.body({ className: 'body', dangerouslySetInnerHTML: { __html: body } });
+    }
+};
+
+
